dca4fcd9c1e68f68627fa7d84b14d84547ff269a
Artem Aleksieiev <artemaleksieiev@Artems-iMac.home>
2023-06-04 23:27:04 -0400

    simenon commit

--- src/components/playlist/Actions.js
+++ src/components/playlist/Actions.js
@@ -1,15 +1,17 @@
-import React from "react";
+import React, { useContext } from "react";
+import playerContext from "../../context/playerContext";
 const fav = () => {
   console.log("I like this one");
 };
 
 const Actions = () => {
+  const { songslist } = useContext(playerContext);
   return (
     <div className="actions">
-      <img src="https://freemusicarchive.org/image?file=images%2Falbums%2FSkidmore_College_Orchestra_-_Mussorgskys_Pictures_at_an_Exhibition_-_2009113013701972.jpg&width=290&height=290&type=image" />
+      <img src="https://audiobookbackend.s3.us-east-2.amazonaws.com/simenon/cover290.jpg" />
       <div className="album_meta">
-        <span className="alb_label">ALBUM</span>
-        <h1>Mussorgsky's Pictures at an Exhibition</h1>
+        <span className="alb_label">AUDIOBOOK</span>
+        <h1>{songslist[0].bookTitle}</h1>
       </div>
       <div className="action_btns">
         <button onClick={() => fav()} className="fav_btn">
